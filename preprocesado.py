c50e258d985d73099d9bc76e88a10e2c557bfb4d
AlejandroHerrera15 <aherrera.rivera12@gmail.com>
2024-05-22 14:33:10 -0500

    calculo inicnial

--- preprocesado.py
+++ preprocesado.py
@@ -9,3 +9,20 @@ tabla_hist=pd.read_csv(tabla_hist)
 sns.histplot(data=tabla_hist, x="NoPaidPerc")
 
 tabla_hist.info()
+tabla_hist.drop(columns=['ID','HomeOwnership', 'Education', 'MaritalStatus'],inplace=True)
+
+from matplotlib.pyplot import figure
+figure(figsize=(20,6))
+sns.heatmap(tabla_hist.corr(),cmap = sns.cubehelix_palette(as_cmap=True), annot = True, fmt = ".2f")
+
+
+tabla_hist['prob_calculada']=(tabla_hist['DebtRatio']*tabla_hist['NoPaidPerc'])*100
+tabla_hist['prob_calculada'].describe()
+
+
+
+
+
+
+
+
